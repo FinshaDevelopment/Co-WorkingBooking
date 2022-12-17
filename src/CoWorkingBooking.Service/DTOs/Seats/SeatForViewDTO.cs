cb27f46b9ce1c664a6a8cfaaf8005b6bcf86b5eb
Temurkhann <109779514+Temurkhann@users.noreply.github.com>
2022-12-18 00:29:52 +0500

    SERVICE: Added View Models

---
+++ src/CoWorkingBooking.Service/DTOs/Seats/SeatForViewDTO.cs
@@ -0,0 +1,18 @@
+﻿using CoWorkingBooking.Service.DTOs.CoWorkings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoWorkingBooking.Service.DTOs.Seats
+{
+    public class SeatForViewDTO
+    {
+        public long Id { get; set; }
+        public int Number { get; set; }
+        public long CoworkingId { get; set; }
+        public CoWorkingForViewDTO CoWorking { get; set; }
+        public bool IsBooked { get; set; }
+    }
+}
