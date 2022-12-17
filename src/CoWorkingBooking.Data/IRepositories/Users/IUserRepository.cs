23439fe6e2e46d10339d157274a8ef735cecd24a
Ulugbek Keldibekov <keldibekovulugbek@gmail.com>
2022-12-17 22:08:16 +0500

    adding repositories

---
+++ src/CoWorkingBooking.Data/IRepositories/Users/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using RaqamliAvlod.Domain.Entities.Users;
+
+namespace RaqamliAvlod.DataAccess.Interfaces.Users
+{
+    public interface IUserRepository : IGenericRepository<User>
+    {
+        public Task<User?> GetByEmailAsync(string email);
+
+        public Task<User?> GetByUsernameAsync(string username);
+
+        public Task<User?> GetByPhonNumberAsync(string phoneNumber);
+    }
+}
