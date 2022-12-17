327333128c453817ff224a9301035d15e367ba49
Ulugbek Keldibekov <keldibekovulugbek@gmail.com>
2022-12-17 23:54:53 +0500

    DATA: Separated repositories

--- src/CoWorkingBooking.Data/IRepositories/Orders/IOrderRepository.cs
+++ src/CoWorkingBooking.Data/IRepositories/Orders/IOrderRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CoWorkingBooking.Domain.Entities.Orders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace CoWorkingBooking.Data.IRepositories.Orders
 {
-    internal class IOrderRepository
+    public interface IOrderRepository : IGenericRepository<Order>
     {
     }
 }
