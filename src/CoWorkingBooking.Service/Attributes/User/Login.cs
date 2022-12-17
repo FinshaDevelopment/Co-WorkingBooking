8b803a56b59850afc7045bb1fddccce3ef563f33
MuhammadKasimov <muhammadmamushah.com@gmail.com>
2022-12-17 21:01:02 +0500

    SERVICE: Added custom validation for user

---
+++ src/CoWorkingBooking.Service/Attributes/User/Login.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoWorkingBooking.Service.Attributes.User
+{
+    public class UserLogin : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+            => value is string login &&
+                login.All(c => char.IsDigit(c) ||
+                char.IsLetter(c) || c == '_') &&
+                login.Any(c => char.IsLetter(c));
+    }
+}
