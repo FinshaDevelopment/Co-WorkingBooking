684a975dc074c3e9819ef00eb0cfbc6b74d58976
MuhammadKasimov <muhammadmamushah.com@gmail.com>
2022-12-17 23:21:55 +0500

    Applied Custom Vlaidations to DTOs

--- src/CoWorkingBooking.Service/Attributes/User/Password.cs
+++ src/CoWorkingBooking.Service/Attributes/User/Password.cs
@@ -9,10 +9,16 @@ namespace CoWorkingBooking.Service.Attributes.User
 {
     public class Password : ValidationAttribute
     {
-        public override bool IsValid(object value)
-            => value is string password &&
+        public override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is string password &&
                 password.Length >= 8 &&
-                    password.Any(c => char.IsDigit(c)) &&
-                        password.Any(c => char.IsLetter(c));
-    }
+                password.Any(c => char.IsDigit(c)) &&
+                password.Any(c => char.IsLetter(c)))
+            {
+                return ValidationResult.Success;
+            }
+            return new ValidationResult("Password should contain at least 8 characters," +
+                                                    " should contain at least on letter and digit");
+        } }
 }
